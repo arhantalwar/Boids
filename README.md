4f62c4f113dca9050e693d519d9bbc9193e8f1b5
arhantalwar <arhantalwar@gmail.com>
2025-04-11 19:00:56 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Boids Algorithm
+
+An implementation of Boids Algorithm which was developed by Craig Reynolds.
+It simulates the flocking behaviour of birds
+
+## QUICK START
+
+Dependencies
+
+- [raylib](https://www.raylib.com/)
+
+## TWEAKS
+
+```c
+    float avoid_factor = 0.01f;
+    float alignment_factor = 0.35f;
+    float cohesion_factor = 0.02f;
+```
+
+https://github.com/user-attachments/assets/5c555544-1932-4390-80eb-cf0ed4ce7a97
+
+### QUICK START
+
+```console
+$ gcc main.c -o main -lraylib -lm
+$ ./main
+```
+
---
+++ main.c
@@ -0,0 +1,182 @@
+#include <raylib.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <raymath.h>
+
+#define GET_VAR(x, y) x##y
+
+#define WIDTH   1760
+#define HEIGHT  990
+
+#define MAX_BOIDS 200
+#define NUM_BOIDS 200
+#define MAX_SPEED 20
+
+#define BOID_RADIUS 60
+
+static int boid_count = 0;
+
+typedef struct Boid {
+    Vector2 position;
+    float radius;
+    float speed_x;
+    float speed_y;
+    Color color;
+} Boid;
+
+Boid* get_boid() {
+    Boid* b = (Boid*) malloc(sizeof(Boid));
+    return b;
+}
+
+void init_boid(Boid* b, Boid** boids_arr) {
+
+    Color colors[] = { MAROON, BLUE, PINK, ORANGE };
+    boids_arr[boid_count++] = b;
+
+    b->radius = BOID_RADIUS;
+    b->position.x = rand() % WIDTH;
+    b->position.y = rand() % HEIGHT;
+    b->speed_x = (rand() % (2 * MAX_SPEED)) - MAX_SPEED;
+    b->speed_y = (rand() % (2 * MAX_SPEED)) - MAX_SPEED;
+    b->color = colors[rand() % 4];
+
+}
+
+int main() {
+
+    srand(time(NULL));
+
+    SetTraceLogLevel(LOG_NONE);
+    SetTargetFPS(60);
+    InitWindow(WIDTH, HEIGHT, "BOIDS");
+
+    Boid* boids_arr[MAX_BOIDS];
+
+    float avoid_factor = 0.01f;
+    float alignment_factor = 0.35f;
+    float cohesion_factor = 0.02f;
+
+    bool draw_dist = false;
+
+    // Generate Boids
+    for(int i = 0; i < NUM_BOIDS; i++) {
+        Boid* GET_VAR(boid, i) = get_boid();
+        init_boid(GET_VAR(boid, i), boids_arr);
+    }
+
+    while(!WindowShouldClose()) {
+
+        if (IsKeyPressed(KEY_R)) {
+            draw_dist = !draw_dist;
+        }
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        for(int i = 0; i < boid_count; i++) {
+
+            float avg_speed_x = 0.0f;
+            float avg_speed_y = 0.0f;
+            int avg_speed_count = 0;
+
+            float avg_position_x = 0.0f;
+            float avg_position_y = 0.0f;
+            int avg_position_count = 0;
+
+            // Drawing Boids
+
+            DrawCircle(boids_arr[i]->position.x, boids_arr[i]->position.y, 7, boids_arr[i]->color);
+            /* DrawCircleLines(boids_arr[i]->position.x, boids_arr[i]->position.y, boids_arr[i]->radius, boids_arr[i]->color); */
+
+            // Implementing Rules
+
+            for(int j = 0; j < boid_count; j++) {
+
+                Vector2 velocity = { boids_arr[i]->speed_x, boids_arr[i]->speed_y };
+
+                if (Vector2Length(velocity) > MAX_SPEED) {
+                    velocity = Vector2Scale(Vector2Normalize(velocity), MAX_SPEED);
+                    boids_arr[i]->speed_x = velocity.x;
+                    boids_arr[i]->speed_y = velocity.y;
+                }
+
+                if (i != j) {
+                    bool in_range = CheckCollisionPointCircle(boids_arr[i]->position, boids_arr[j]->position, boids_arr[j]->radius);
+                    if (in_range) {
+
+                        if (draw_dist) DrawLineEx(boids_arr[i]->position, boids_arr[j]->position, 1.0f, RAYWHITE);
+
+                        // RULE 1 : SEPERATION
+                        Vector2 move = Vector2Subtract(boids_arr[i]->position, boids_arr[j]->position);
+
+                        boids_arr[i]->speed_x += move.x * avoid_factor;
+                        boids_arr[i]->speed_y += move.y * avoid_factor;
+
+                        avg_speed_x += boids_arr[j]->speed_x;
+                        avg_speed_y += boids_arr[j]->speed_y;
+                        avg_speed_count++;
+
+                        avg_position_x += boids_arr[j]->position.x;
+                        avg_position_y += boids_arr[j]->position.y;
+                        avg_position_count++;
+
+                    }
+                }
+            }
+
+            // RULE 2 : ALIGNMENT
+            if (avg_speed_count > 0) {
+
+                avg_speed_x /= avg_speed_count;
+                avg_speed_y /= avg_speed_count;
+
+                boids_arr[i]->speed_x += (avg_speed_x - boids_arr[i]->speed_x) * alignment_factor;
+                boids_arr[i]->speed_y += (avg_speed_y - boids_arr[i]->speed_y) * alignment_factor;
+
+            }
+
+
+            // RULE 3 : COHESION
+            if (avg_position_count > 0) {
+
+                avg_position_x /= avg_position_count;
+                avg_position_y /= avg_position_count;
+
+                Vector2 direction_to_center = {
+                    .x = avg_position_x - boids_arr[i]->position.x,
+                    .y = avg_position_y - boids_arr[i]->position.y
+                };
+
+                boids_arr[i]->speed_x += direction_to_center.x * cohesion_factor;
+                boids_arr[i]->speed_y += direction_to_center.y * cohesion_factor;
+
+            }
+
+            // Bound check
+
+            if (boids_arr[i]->position.x < 0) boids_arr[i]->position.x = WIDTH;
+            if (boids_arr[i]->position.x > WIDTH) boids_arr[i]->position.x = 0;
+            if (boids_arr[i]->position.y < 0) boids_arr[i]->position.y = HEIGHT;
+            if (boids_arr[i]->position.y > HEIGHT) boids_arr[i]->position.y = 0;
+
+            boids_arr[i]->position.x += boids_arr[i]->speed_x * alignment_factor;
+            boids_arr[i]->position.y += boids_arr[i]->speed_y * alignment_factor;
+
+        }
+
+        EndDrawing();
+
+    }
+
+    CloseWindow();
+
+    // Free Boids
+    for(int i = 0; i < boid_count; i++) {
+        free(boids_arr[i]);
+    }
+
+    return 0;
+
+}
